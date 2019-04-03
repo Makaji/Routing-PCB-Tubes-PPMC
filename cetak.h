820a9d936382e1098c825eb70a52d662d496d8b6
rebeccaviona <49097773+rebeccaviona@users.noreply.github.com>
2019-04-03 18:09:01 +0700

    Update cetak.h

--- cetak.h
+++ cetak.h
@@ -3,7 +3,7 @@
 
 // Deklarasi fungsional untuk mencetak Layout dan Routing
 
-void cetakLayout (char tampillayout[40][40][2], int m, int n);
-void cetakRouting (char tampilRouting [40][40], int n, int n);
+void cetakLayout (char L[42][42][2], int m, int n);
+void cetakRouting (char R[42][42], int m, int n);
 
 #endif
