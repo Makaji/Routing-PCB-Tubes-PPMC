c1f2b5a525af8050c63d48b2c88389859b988c27
rebeccaviona <49097773+rebeccaviona@users.noreply.github.com>
2019-04-08 19:15:30 +0700

    Add files via upload

--- cetak.c
+++ cetak.c
@@ -1,39 +1,51 @@
-//**********************************************************//
-//						[Source Code]						//
-//															//
-// Institution		: Institut Teknologi Bandung			//
-// Name				: <Nama Praktikan>						//
-// File Name		: mode.c								//
-// Dependency		: stdio.h, string.h, stdlib.h, main.h,	//
-// cetak.h, layout.h, routing.h								//
-//															//
-// Description:												//
-//	This program executes the features that is included in	//
-//	the whole program after a file is declared in menu.		//
-//	The program returns after the operator is inputted to 	//
-//	save and exit											//
-// 															//
-// Status:													//
-// 1. <Nama Praktikan>	- <NIM>    : Membuat file			//
-// 2. Gede Satya A D 	- 13217016 : Fix the bug where num-	//
-//	  ber 2 is printed twice								//
-//															//
-//**********************************************************//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "cetak.h"
 
-void cetakLayout (char layout[42][42][2], int M, int N){
-	// Deklarasi variabel lokal
+int i, j, m, n;
+char L[42][42][2];
+char R[42][42];
 
-	// Algoritma fungsional cetak Layout
-
+void cetakLayout(){
+    printf("  ");
+    for (i=1;i<=m;i++){
+        printf("%10d",(i));
+    }
+    printf("\n");
+    for (i=1;i<=n;i++){
+        if (i<10){
+            for (j=0;j<1;j++){
+                printf("%d",i);
+            }
+            for (j=1;j<=m;j++){
+                printf(" %10s",L[i][j]);
+            }
+        } else{
+            for (j=0;j<1;j++){
+                printf("%d",i);
+            }
+            for (j=1;j<=m;j++){
+                printf("%10s",L[i][j]);
+            }
+        }
+        printf ("\n");
+    }
 }
 
-void cetakRouting (char routing [42][42], int M, int N){
-	// Deklarasi variabel lokal
-
-	// Algoritma fungsional cetak Routing
-
+void cetakRouting(){
+    //R[10][1]='@'; buat ngetes doang
+    //R[10][10]='*'; buat ngetes doang
+    printf("  ");
+    for (i=0;i<m;i++){
+        printf("%10d",(i+1));
+    }
+    printf("\n");
+    for (i=1;i<=n;i++){
+        printf("%d",i);
+        for (j=1;j<=m;j++){
+            printf("%10c",R[i][j]);
+        }
+        printf("\n");
+    }
 }
